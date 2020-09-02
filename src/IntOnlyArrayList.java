2fc477464a052466c0b5488fc7da9d97d1e4a5a2
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2020-09-02 15:58:04 +0200

    bla

--- src/IntOnlyArrayList.java
+++ src/IntOnlyArrayList.java
@@ -2,28 +2,48 @@ import java.util.Arrays;
 
 public class IntOnlyArrayList {
 
-    private int[] ia = new int[0];
+    private int[] intArray = new int[0];
 
     public IntOnlyArrayList(){
-        int[] ia = new int[0];
+        int[] intArray = new int[0];
     }
 
+    // Tager brug af copyOf metoden fra java util library til at kopierer den gamle array og indsætter værdi for enden
     public void add(int value){
-        ia = Arrays.copyOf(ia, ia.length + 1);
-        ia[ia.length - 1] = value;
-    }
-    public void remove(int index) {
-        int[] ia_new = Arrays.copyOf(ia, ia.length - 1);
-        for (int i = index; i < ia_new.length; i++) {
-            ia_new[i] = ia[i+1];
-        }
-        ia = ia_new;
+        intArray = Arrays.copyOf(intArray, intArray.length + 1);
+        intArray[intArray.length - 1] = value;
     }
 
-    public void print(){
-        System.out.println(Arrays.toString(ia));
+    // Fjerner værdien fra det valgte index
+    public void remove(int index) {
+        int[] intArray_new = Arrays.copyOf(intArray, intArray.length - 1);
+        for (int i = index; i < intArray_new.length; i++) {
+            intArray_new[i] = intArray[i+1];
+        }
+        intArray = intArray_new;
     }
+    // Kopierer array indtil den når valgte index, bytter den ud med valgte værdi og kopierer resten af array
+    public void addSpecific(int value, int index) {
+
+        int[] intArray_new = Arrays.copyOf(intArray, intArray.length);
+
+        for (int i = 0; i < index + 1; i++) {
+            if (i < index - 1)
+                intArray_new[i] = intArray[i];
+            else if (i == index - 1)
+                intArray_new[i] = value;
+            else
+                intArray_new[i] = intArray[i - 1];
+        }
+        intArray = intArray_new;
+    }
+    // Bruger toString metoden fra java util library til at printe array som en string
+    public void print(){
+        System.out.println(Arrays.toString(intArray));
+    }
+
+    // Getter til at hente valgte index og printe i konsol
     public int getElement(int index){
-        return ia[index];
+        return intArray[index];
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         IntOnlyArrayList intOnlyArrayList = new IntOnlyArrayList();
 
+        // Kopierer arrayen 6 gange og tilføjer en værdi for hver kopi
         intOnlyArrayList.add(5);
         intOnlyArrayList.add(2);
         intOnlyArrayList.add(7);
@@ -10,12 +11,23 @@ public class Main {
         intOnlyArrayList.add(32);
         intOnlyArrayList.add(13);
 
+        // Henter værdien på index 2
         System.out.println(intOnlyArrayList.getElement(2));
+
+        // Printer array til konsol
+        intOnlyArrayList.print();
+
+        // Fjerner værdi på index 2
+        intOnlyArrayList.remove(2);
 
         intOnlyArrayList.print();
 
-        intOnlyArrayList.remove(2);
+        // Kopierer array og indsætter værdien 53131 på index 4 i den nye array
+        intOnlyArrayList.addSpecific(53131,4);
+        intOnlyArrayList.print();
 
+        // Kopierer array og indsætter værdien 312321 på index 1 i den nye array
+        intOnlyArrayList.addSpecific(312321,1);
         intOnlyArrayList.print();
     }
 }
